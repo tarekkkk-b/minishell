382f286a5ce28a4a4b884d78f6071278c011b603
Tarek Badawi <tabadawi@student.42abudhabi.ae>
2024-06-12 11:56:45 +0400

    structure started,,, /notes/format.txt

--- mini_shell/srcs/minishell.c
+++ mini_shell/srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: tabadawi <tabadawi@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 11:14:30 by tabadawi          #+#    #+#             */
-/*   Updated: 2024/06/11 19:47:43 by tabadawi         ###   ########.fr       */
+/*   Updated: 2024/06/12 11:56:10 by tabadawi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,6 @@ int	main(int ac, char **av, char **env)
 	char *path;
 	char *accpath;
 	char *str;
-	int	i;
-	int	pipe_count;
-	char ***pipes;
-	char **one_command;
 	(void)av;
 	(void)env;
 	if (ac != 1)
@@ -33,29 +29,6 @@ int	main(int ac, char **av, char **env)
 		str = readline(accpath);
 		if (str[0] != '\0')
 			add_history(str);
-		i = 0;
-		pipe_count = 0;
-		while (str[i])
-		{
-			if (str[ft_strlen(str) - 1] == '|')
-			{
-				printf("ERROR\n");
-				exit(0);
-			}
-			if (str[i] == '|')
-			{
-				if (str[i + 1] != '|')
-					pipe_count++;
-				else
-				{
-					printf("ERROR\n");
-					exit(0);
-				}		
-			}
-			i++;
-		}
-		// printf("%d\n", pipe_count);
-		pipes = malloc(sizeof(char **) * pipe_count + 1);
 		free (str);
 	}
 }
