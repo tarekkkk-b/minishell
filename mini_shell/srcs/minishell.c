94e7b24da26a881eee449540cc0b219ad63c6dfe
Tarek Badawi <tabadawi@student.42abudhabi.ae>
2024-06-14 21:36:57 +0400

    structs? (started)

--- mini_shell/srcs/minishell.c
+++ mini_shell/srcs/minishell.c
@@ -6,29 +6,51 @@
 /*   By: tabadawi <tabadawi@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 11:14:30 by tabadawi          #+#    #+#             */
-/*   Updated: 2024/06/12 11:56:10 by tabadawi         ###   ########.fr       */
+/*   Updated: 2024/06/14 21:36:33 by tabadawi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+typedef struct s_env
+{
+	int		exit;
+	int		shlvl;
+	char	*cwd;
+	char	*owd;
+	char	**env;
+	char	**path;
+}	t_env;
+
+typedef	struct s_shell
+{
+	t_env	*env;
+}	t_shell;
+
+
+
 int	main(int ac, char **av, char **env)
 {
-	char *path;
-	char *accpath;
-	char *str;
+	t_shell	shell;
+
+	shell.env = malloc(sizeof(t_env));
 	(void)av;
-	(void)env;
 	if (ac != 1)
 		return (-1);
 	printf("\e[1;1H\e[2J");
+
+	char *test = getenv("SHLVL");
+	printf("%s\n\n", test);
+
+
+	
 	while (1)
 	{
-		path = getcwd(NULL, 0);
-		accpath = ft_strjoin(path, "> ");
-		str = readline(accpath);
-		if (str[0] != '\0')
-			add_history(str);
-		free (str);
+		// path = getcwd(NULL, 0);
+		// accpath = ft_strjoin(path, "> ");
+		// str = readline(accpath);
+		// if (str[0] != '\0')
+		// 	add_history(str);
+		// free (str);
 	}
 }
